05b53d3f35f6cce597bad226d78d4b1ec7ae9d72
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-10-15 23:59:57 +0200

    add rotating text in about me component and change project image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,9 +3,12 @@ import './AboutMe.css'
 import CV from '../files/CV - Patricia Jurado De Bilbao_EN.pdf'
 import Slide from 'react-reveal/Slide';
 import Fade from 'react-reveal/Fade'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const AboutMe = () => {
+    const heart = <FontAwesomeIcon icon={faHeart} color="#db8282" size="sm" />
+
     return (
         <section className="about-container">
             <Fade left duration={1500}>
@@ -14,9 +17,20 @@ const AboutMe = () => {
             <Slide bottom duration={2000}>
                 <article className="about">
 
-                    <p className="about-text">Decidí aprender a programar por mi cuenta en mi tiempo libre y luego me formé en Ironhack. </p>
-                    <p className="about-text">Me encanta ir de ruta en moto, ver series y pelis para quedarme dormida en el sofá con Link y Ganon. </p>  
-                
+                    <p className="about-text">En mi tiempo libre me propuse aprender a programar y me gustó tanto que decidí de formarme en <b>Ironhack</b>. </p>
+                    <p className="about-text">Venezolana viviendo en Madrid hace 3 años. Buscando mi primera experiencia como programadora.</p>  
+                    <div className="outer">
+                        <h1 className="rotating-text">{heart} &nbsp;
+                            <div className="inner">
+                                <span>
+                                    ir de ruta en moto<br />
+                                    mis gatitos Link y Ganon<br />
+                                    ver pelis y series<br />
+                                    música a todo volumen
+                                </span>
+                            </div>
+                        </h1>
+                    </div>
                     <div className="CV-container">
                         <a className="download-button" download="CV Patricia Jurado De Bilbao" href={CV}>Descargar CV</a>
                     </div>
@@ -30,6 +44,7 @@ const AboutMe = () => {
                         </svg>
                     </div>
         </section>
+
         
     )
 }
