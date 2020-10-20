944d1c4cb31161ee9bd34a8bdf630ca7fa4a4990
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-10-20 20:31:45 +0200

    add final changes in project card hover

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -15,14 +15,13 @@ const ProjectCard = props => {
     return (
         <Col lg={4} md={6}>
         <HeadShake>
-            <Card className="card">
-                <div className="card-first">
-                    <img className="bg-image" src={props.image} alt={props.name} />
+            {/* <Card className="card">
                     <h1 className="card-title">{props.name}</h1>
-                </div>
-                <div className="info">
                     <Card.Text className="project-description">{props.description}</Card.Text>
                     <Card.Text className="project-tech">{props.tech}</Card.Text>
+
+                <div className="info">
+                    <img className="bg-image" src={props.image} alt={props.name} />
                     <div className="links-container">
                         <ul className="links">
                             <li><a href={props.github} title="Ver repositorio" target="blank">{code}</a></li>
@@ -30,6 +29,24 @@ const ProjectCard = props => {
                         </ul>
                     </div>
                 </div>
+            </Card> */}
+            <Card className="card">
+               <div className="card-img-wrapper">
+                    <img className="card-img-top" src={props.image} alt={props.name} />
+               </div>
+               <div className="card-body">
+                    <h1 className="card-title">{props.name}</h1>
+                    <Card.Text className="project-tech">{props.tech}</Card.Text>
+                    <div className="card-content">
+                        <Card.Text className="project-description card-text">{props.description}</Card.Text>
+                        <div className="links-container">
+                            <ul className="links">
+                                <li><a href={props.github} title="Ver repositorio" target="blank">{code}</a></li>
+                                <li><a href={props.url} title="Ver web" target="blank">{laptop}</a></li>
+                            </ul>
+                        </div>
+                    </div>
+               </div> 
             </Card>
         </HeadShake>
         </Col>
